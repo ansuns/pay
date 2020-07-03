68a8c704436f005d2fa6c255f3d9ea99c01c59c4
阿深 <251024883@qq.com>
2020-07-03 10:36:20 +0800

    test

---
+++ SandPay.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ANSUNS
+ * Date: 2020/7/3
+ * Time: 10:33
+ */
+
+namespace Ansuns;
+
+
+class SandPay
+{
+    public static function tests()
+    {
+        echo "sucess";
+    }
+
+}
