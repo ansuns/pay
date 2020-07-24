5377c9e6e9a000224e74608314235b260772cab3
阿深 <251024883@qq.com>
2020-07-24 21:11:02 +0800

    优化JSAPI支付配置

--- src/Gateways/Sandpay/Miniapp.php
+++ src/Gateways/Sandpay/Miniapp.php
@@ -19,56 +19,32 @@ class Miniapp extends Sandpay
      */
     protected function getTradeType()
     {
-        return 'MINIAPP';
+        return 'trade.create';
     }
 
-    /**
-     * 应用并返回参数
-     * @param array $options
-     * @return array
-     * @throws \Ansuns\Pay\Exceptions\GatewayException
-     */
     public function apply(array $options = [])
     {
-        $this->service = "/qr/jsapiScan";
-        $this->setReqData([
-            'ordNo' => $options['out_trade_no'] ?? ToolsService::get_bill_number(),
-            'amt' => ToolsService::ncPriceFen2yuan($options['total_fee']),
-            'payType' => 'LETPAY',
-            'timeExpire' => 3,
-            'subject' => '微信小程序买单',
-            'subOpenid' => $options['openid'],
-            'subAppid' => $options['appid'], //wxfb6c194c44d7cc1f
-            'notifyUrl' => str_replace('https', 'http', $options['notify_url']),
-            //'subMchId'     => $this->userConfig->get('sub_mch_id'),//必传
-            //'attach'       => '{}',
-        ]);
+        $this->config['method'] = $this->getTradeType();
+        $this->setReqData($options);
         $result = $this->getResult();
+
         if ($this->isSuccess($result)) {
-            $pay_request = [
-                'appId' => $result['payAppId'],
-                'timeStamp' => $result['payTimeStamp'],
-                'nonceStr' => $result['paynonceStr'],
-                'signType' => $result['paySignType'],
-                'package' => $result['payPackage'],
-                'paySign' => $result['paySign'],
-            ];
-            return $pay_request;
+            return $this->buildPayResult($result);
         } else {
-            //特定失败自动尝试配置APPID
-            if (isset($result['bizMsg']) && $result['bizMsg'] == '交易失败，请联系客服' && isset($result['bizCode']) && $result['bizCode'] == '2010') {
-                try {
-                    $this->service = "/weChat/bindconfig";
-                    $this->config['reqData'] = [];
-                    $this->setReqData([
-                        'mno' => $this->userConfig->get('mno', ''),
-                        'subMchId' => $this->userConfig->get('sub_mch_id'),//必传
-                        'subAppid' => $options['appid'],//必传
-                    ]);
-                    $this->getResult();
-                } catch (\Exception $e) {
-                    wr_log($e->getMessage());
-                }
+            try {
+                //特定失败自动尝试配置APPID
+                $this->gateway = $this->gatewayAgent;
+                $this->config['method'] = 'merchant.bind.wechat.appid';
+                $this->config['biz_content'] = [];
+                $this->setReqData([
+                    'sub_merchant_id' => $this->config['sub_app_id'],
+                    'sub_appid' => $options['mer_app_id'],//必传
+                ]);
+
+                $this->getResult();
+
+            } catch (\Exception $e) {
+                return $e->getMessage();
             }
         }
         return $result;
