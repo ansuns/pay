b5a9daae0d41a278d76877ccd084a46e12308974
阿深 <251024883@qq.com>
2020-08-14 08:02:00 +0800

    tt

--- src/Service/ToolsService.php
+++ src/Service/ToolsService.php
@@ -1699,4 +1699,20 @@ class ToolsService
         }
         return $zip->close();
     }
+
+    /**
+     *
+     * @return string
+     */
+    public static function createAccessMerchId()
+    {
+        $charid = md5(uniqid(mt_rand(), true));
+        $hyphen = chr(45);// "-"
+        $uuid = substr($charid, 0, 8) . $hyphen
+            . substr($charid, 8, 4) . $hyphen
+            . substr($charid, 12, 4) . $hyphen
+            . substr($charid, 16, 4) . $hyphen
+            . substr($charid, 20, 12);
+        return $uuid;
+    }
 }
