0944cffb9f1722f1e2d671b4400b829cf21311ce
阿深 <251024883@qq.com>
2020-09-07 17:49:10 +0800

    fix

--- src/Service/ToolsService.php
+++ src/Service/ToolsService.php
@@ -1715,4 +1715,56 @@ class ToolsService
             . substr($charid, 20, 12);
         return $uuid;
     }
+
+    /**
+     * 数组转换成XML数据
+     * @param array $array
+     * @return string
+     * @throws WechatException
+     */
+    public static function arrayToXml($array)
+    {
+        if (!is_array($array)) {
+            throw new \Exception('`$arr`不是有效的array。');
+        }
+        $xml = "<xml>\r\n";
+        $xml .= self::arrayToXmlSub($array);
+        $xml .= "</xml>";
+        return $xml;
+    }
+
+    private static function arrayToXmlSub($array)
+    {
+        if (!is_array($array)) {
+            throw new \Exception('`$array`不是有效的array。');
+        }
+        $xml = "";
+        foreach ($array as $key => $val) {
+            if (is_array($val)) {
+                if (is_numeric($key)) {
+                    $xml .= self::arrayToXmlSub($val);
+                } else {
+                    $xml .= "<" . $key . ">" . self::arrayToXmlSub($val) . "</" . $key . ">\r\n";
+                }
+            } elseif (is_numeric($val)) {
+                $xml .= "<" . $key . ">" . $val . "</" . $key . ">\r\n";
+            } else {
+                $xml .= "<" . $key . "><![CDATA[" . $val . "]]></" . $key . ">\r\n";
+            }
+        }
+        return $xml;
+    }
+
+    /**
+     * XML数据转换成array数组
+     * @param string $xml
+     * @return array
+     */
+    public static function xmlToArray($xml)
+    {
+        // 禁止引用外部xml实体
+        libxml_disable_entity_loader(true);
+        $res = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+        return (array)$res;
+    }
 }
