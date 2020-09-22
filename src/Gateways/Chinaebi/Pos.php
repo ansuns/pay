e6d55e936a9fe24b9df8a70112447074b0688718
阿深 <251024883@qq.com>
2020-09-22 17:02:12 +0800

    生成电银动态二维码(C 扫 B)

--- src/Gateways/Chinaebi/Pos.php
+++ src/Gateways/Chinaebi/Pos.php
@@ -24,34 +24,15 @@ class Pos extends Chinaebi
     }
 
     /**
-     * 应用并返回参数
+     * code_url，生成电银动态二维码(C 扫 B)
      * @param array $options
      * @return array
      * @throws \Ansuns\Pay\Exceptions\GatewayException
      */
     public function apply(array $options = [])
     {
-        $this->service = '/qr/reverseScan';
-        $auth_code = $options['auth_code'];
-        $this->setReqData([
-                'authCode' => $auth_code,
-                'ordNo' => $options['out_trade_no'],
-                'subject' => $options['body'],
-                'amt' => ToolsService::ncPriceFen2yuan($options['total_fee']),
-            ]
-        );
-        if (preg_match("/^(10|11|12|13|14|15)\d{16}$/", $auth_code)) {
-            $this->setReqData(['payType' => 'WECHAT']);//10~15开头18位 微信付款码
-        } elseif (preg_match("/^28\d{15,16}$/", $auth_code)) {
-            $this->setReqData(['payType' => 'ALIPAY']); //17位是分期码 28开头 18位支付宝付款码
-        } else {
-            throw new \Exception('付款码格式有误,请核实是否微信或支付宝付款码!');
-        }
-        $result = $this->getResult();
-        if ($this->isSuccess($result)) {
-            return $this->buildPayResult($result);
-        }
-        $result['trade_state'] = ($result['err_code'] == '2068') ? 'USERPAYING' : 'PAYERROR'; //只要不是支付中,则认为支付失败
+        $this->setReqData($options);
+        return $this->getResult();
         return $result;
     }
 }
