0c0421a595daf0bfaacd8bc1945acfcb9c409565
阿深 <251024883@qq.com>
2020-10-05 11:51:19 +0800

    易生支付-1

---
+++ src/Gateways/Bhecard.php
@@ -0,0 +1,402 @@
+<?php
+
+namespace Ansuns\Pay\Gateways;
+
+use Ansuns\Pay\Contracts\Config;
+use Ansuns\Pay\Contracts\GatewayInterface;
+use Ansuns\Pay\Exceptions\GatewayException;
+use Ansuns\Pay\Exceptions\InvalidArgumentException;
+use Ansuns\Pay\Service\ToolsService;
+use GuzzleHttp\Client;
+
+/**
+ * 易生支付基础类
+ * Class Bhecard
+ * @package Pay\Gateways\Bhecard
+ */
+abstract class Bhecard extends GatewayInterface
+{
+
+    /**
+     * @var array
+     */
+    protected $config;
+    /**
+     * @var string
+     */
+    protected $service;
+    /**
+     * @var Config
+     */
+    protected $userConfig;
+
+    /**
+     * @var string
+     */
+    protected $gateway = "https://test_nucc.bhecard.com:9088/api_gateway.do";
+
+
+    /**
+     * Wechat constructor.
+     * @param array $config
+     * @throws \Exception
+     */
+    public function __construct(array $config)
+    {
+        $this->userConfig = new Config($config);
+        $this->config = [
+            'service' => '',
+            'partner' => $this->userConfig->get('partner', ''),
+            'sign' => '',
+            'sign_type' => 'RSA',
+            'charset' => 'UTF-8',
+            'biz_content' => [],
+        ];
+    }
+
+    /**
+     * 设置请求数据
+     * @param array $array
+     * @return $this
+     */
+    protected function setReqData($array)
+    {
+        $this->config['biz_content'] += $array;
+        return $this;
+    }
+
+    /**
+     * 获取验证访问数据
+     * @return array
+     * @throws GatewayException
+     */
+    protected function getResult()
+    {
+        $this->config['service'] = $this->service;
+        $this->config['sign'] = $this->getSign($this->config['biz_content']);
+        $this->config['biz_content'] = json_encode($this->config['biz_content'], JSON_UNESCAPED_UNICODE);
+        $client = new Client(['verify' => false]);
+        $return_data = $client->request('POST', $this->gateway, ['form_params' => $this->config])->getBody()->getContents();
+        $service_return_name = str_replace(".", "_", $this->service) . '_response';
+        $result = json_decode($return_data, true);
+        $trade_response = json_encode($result[$service_return_name], 256);
+
+        if (!ToolsService::is_json($trade_response)) {
+            throw new GatewayException('返回结果不是有效json格式', 20000, $result);
+        }
+        $result = json_decode($trade_response, true);
+
+//        if (!empty($result['sign']) && !$this->verify($this->getSignContent($result), $result['sign'], $this->userConfig->get('sxf_pub_key'))) {
+//            throw new GatewayException('验证签名失败', 20000, $result);
+//        }
+        $response_data = $result;
+        $response_data['rawdata'] = $return_data;
+        $response_data['return_code'] = 'SUCCESS'; //数据能解析则通信结果认为成功
+        $response_data['result_code'] = 'SUCCESS'; //初始状态为成功,如果失败会重新赋值
+        $response_data['return_msg'] = isset($response_data['msg']) ? $response_data['msg'] : 'OK!';
+        if (!isset($result['code']) || $result['code'] !== '00') {
+            $response_data['result_code'] = 'FAIL';
+            $response_data['err_code'] = isset($response_data['code']) ? $response_data['code'] : '';
+            $response_data['err_code_des'] = isset($response_data['msg']) ? $response_data['msg'] : '';
+        }
+        return $response_data;
+    }
+
+    /**
+     * 订单退款操作
+     * @param array $options
+     * @return array
+     * @throws GatewayException
+     */
+    public function refund($options = [])
+    {
+        $this->service = "/qr/refund";
+        $this->setReqData([
+            'ordNo' => $options['out_refund_no'],
+            'origOrderNo' => $options['out_trade_no'],
+            'amt' => ToolsService::ncPriceFen2yuan($options['refund_fee']),
+        ]);
+        $data = $this->getResult();
+        if ($this->isSuccess($data)) {
+            $return = [
+                'return_code' => $data['return_code'], //通信结果
+                'return_msg' => $data['return_msg'],
+                'result_code' => $data['result_code'],
+                'appid' => isset($data['appId']) ? $data['appId'] : '',
+                'mch_id' => '',
+                'nonce_str' => isset($data['nonceStr']) ? $data['nonceStr'] : '',
+                'sign' => isset($data['sign']) ? $data['sign'] : '',
+                'out_refund_no' => $data['ordNo'],
+                'out_trade_no' => $data['origOrderNo'],
+                'refund_id' => '',
+                'transaction_id' => '',
+                'refund_fee' => ToolsService::ncPriceYuan2fen($data['refundAmount']),  //元转分
+                'raw_data' => $data
+            ];
+            return $return;
+        }
+        return $data;
+    }
+
+    /**
+     * 查询退款订单状态
+     * @param string $out_trade_no
+     * @return array
+     * @throws GatewayException
+     */
+    public function refund_find($out_trade_no = '')
+    {
+        $this->service = "/qr/tradeRefundQuery";
+        $this->setReqData(['ordNo' => $out_trade_no]);
+        $data = $this->getResult();
+        if ($this->isSuccess($data)) {
+            $return = [
+                'return_code' => $data['return_code'], //通信结果
+                'return_msg' => $data['return_msg'],
+                'result_code' => $data['result_code'],
+                'appid' => isset($data['appId']) ? $data['appId'] : '',
+                'mch_id' => '',
+                'nonce_str' => isset($data['nonceStr']) ? $data['nonceStr'] : '',
+                'sign' => isset($data['sign']) ? $data['sign'] : '',
+                'out_refund_no' => $data['ordNo'],
+                'out_trade_no' => $out_trade_no,
+                'refund_id' => '',
+                'transaction_id' => '',
+                'refund_fee' => ToolsService::ncPriceYuan2fen($data['refundAmount']),  //元转分
+                'raw_data' => $data
+            ];
+            return $return;
+        }
+        return $data;
+    }
+
+    /**
+     * 关闭正在进行的订单
+     * @param string $out_trade_no
+     * @param string $reason
+     * @return array
+     * @throws GatewayException
+     */
+    public function close($out_trade_no = '', $reason = '')
+    {
+        $this->service = "/close";
+        $this->config['outTradeNo'] = $out_trade_no;
+        $this->config['reason'] = $reason;
+        $this->config['merchantCode'] = $this->userConfig->get('merchant_no');
+        return $this->getResult();
+    }
+
+    /**
+     * 查询订单状态OK
+     * @param string $out_trade_no
+     * @return array
+     * @throws GatewayException
+     */
+    public function find($out_trade_no = '')
+    {
+        $this->service = "/qr/query";
+        $this->setReqData(['ordNo' => $out_trade_no]);
+        $data = $this->getResult();
+        if ($this->isSuccess($data)) {
+            return $this->buildPayResult($data);
+        }
+        $trade_state = $data['tranSts'] ?? 'FAIL';
+        $data['trade_state'] = ($trade_state == 'USER_PAYING') ? 'USERPAYING' : $trade_state;
+        return $data;
+    }
+
+    protected function buildPayResult($data)
+    {
+        $return = [
+            'return_code' => $data['return_code'], //通信结果
+            'return_msg' => $data['return_msg'],
+            'result_code' => $data['result_code'],
+            'appid' => isset($data['appId']) ? $data['appId'] : '',
+            'mch_id' => '',
+            'device_info' => '',
+            'nonce_str' => isset($data['nonceStr']) ? $data['nonceStr'] : '',
+            'sign' => isset($data['sign']) ? $data['sign'] : '',
+            'openid' => isset($data['buyerId']) ? $data['buyerId'] : '',
+            'is_subscribe' => '',
+            'trade_type' => isset($data['payType']) ? $data['payType'] : '',
+            'bank_type' => '',
+            'total_fee' => ToolsService::ncPriceYuan2fen(isset($data['oriTranAmt']) ? $data['oriTranAmt'] : $data['buyerPayAmount']),  //分
+            'transaction_id' => isset($data['transactionId']) ? $data['transactionId'] : '',
+            'out_trade_no' => isset($data['ordNo']) ? $data['ordNo'] : '',
+            'attach' => '',
+            //'time_end'       => ToolsService::format_time($data['payTime']),
+            'time_end' => isset($data['payTime']) ? $data['payTime'] : '',
+            'trade_state' => isset($data['tranSts']) ? $data['tranSts'] : $data['result_code'],
+            'raw_data' => $data
+        ];
+        return $return;
+    }
+
+    /**RSA验签
+     * @param array $data 待签名数据
+     * @param string|null $sign 需要验签的签名
+     * @param bool|string $pub_key
+     * @return bool 验签是否通过 bool值
+     */
+    public function verify($data, $sign = null, $pub_key = false)
+    {
+        $str = chunk_split($pub_key, 64, "\n");
+        $public_key = "-----BEGIN PUBLIC KEY-----\n$str-----END PUBLIC KEY-----\n";
+        //转换为openssl格式密钥
+        $res = openssl_get_publickey($public_key);
+        //调用openssl内置方法验签，返回bool值
+        $result = (bool)openssl_verify($data, base64_decode($sign), $public_key);
+        //释放资源
+        openssl_free_key($res);
+        //返回资源是否成功
+        return $result;
+    }
+
+    /**
+     * @return mixed
+     */
+    abstract protected function getTradeType();
+
+    /**
+     * @param array $options
+     * @return array
+     * @throws GatewayException
+     */
+    protected function preOrder($options = [])
+    {
+        $this->config = array_merge($this->config, $options);
+        return $this->getResult();
+    }
+
+    /**
+     * 判断结果是否成功
+     * @param $result
+     * @return bool
+     */
+    protected function isSuccess($result)
+    {
+        if (!is_array($result)) {
+            return false;
+        }
+        return isset($result['return_code']) && ($result['return_code'] === 'SUCCESS') && isset($result['result_code']) && ($result['result_code'] === 'SUCCESS');
+    }
+
+    /**
+     * 获取微信支付成功通知回复内容
+     * @return string
+     */
+    public function getNotifySuccessReply()
+    {
+        return json_encode(['code' => 'success', 'msg' => '成功']);
+    }
+
+    /**
+     * 返回失败通知
+     * @return string
+     */
+    public function getNotifyFailedReply()
+    {
+        return json_encode(['code' => 'fail', 'msg' => '失败']);
+    }
+
+    /**
+     * 获取微信支付通知
+     * @return array
+     * @throws \Exception
+     */
+    public function getNotify()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        if (!empty($data['sign'])) {
+            $sign = $data['sign'];
+            unset($data['sign']);
+            if (!$this->verify($this->getSignContent($data), $sign, $this->userConfig->get('sxf_pub_key'))) {
+                throw new \Exception('Invalid Notify Sign is error.', '0');
+            }
+            $return = [
+                'return_code' => 'SUCCESS', //通信结果
+                'return_msg' => $data['bizMsg'],
+                'result_code' => $data['bizCode'] == '0000' ? 'SUCCESS' : 'FAIL',
+                'appid' => '',
+                'mch_id' => '',
+                'device_info' => '',
+                'nonce_str' => '',
+                'sign' => $sign,
+                'openid' => $data['buyerId'] ?? $data['openid'],
+                'is_subscribe' => '',
+                'trade_type' => 'trade_type',
+                'bank_type' => '',
+                'total_fee' => ToolsService::ncPriceYuan2fen($data['amt']),  //分
+                'transaction_id' => $data['transactionId'],
+                'out_trade_no' => $data['ordNo'],
+                'attach' => '',
+                //'time_end'       => ToolsService::format_time($data['payTime']),
+                'time_end' => $data['payTime'],
+                'trade_state' => ($data['bizMsg'] == '交易成功') ? 'SUCCESS' : 'FAIL',
+                'raw_data' => $data
+            ];
+            if ($data['bizCode'] !== '0000') {
+                $return['err_code'] = isset($data['subCode']) ? $data['subCode'] : '';
+                $return['err_code_des'] = isset($data['subMsg']) ? $data['subMsg'] : '';
+            }
+            return $return;
+        }
+        exit();
+    }
+
+    /**
+     * 生成内容签名
+     * @param $data
+     * @return string
+     */
+    protected function getSign($data)
+    {
+        if (is_null($this->userConfig->get('sign_key'))) {
+            throw new InvalidArgumentException('Missing Config -- [sign_key]');
+        }
+        $data = $this->getSignContent($data);
+        $cooprator_pri_key = $this->userConfig->get('sign_key');
+        $str = chunk_split($cooprator_pri_key, 64, "\n");
+        $private_key = "-----BEGIN RSA PRIVATE KEY-----\n$str-----END RSA PRIVATE KEY-----\n";
+        $res = openssl_get_privatekey($private_key);
+        openssl_sign($data, $sign, $res);
+        openssl_free_key($res);
+        return base64_encode($sign);  //base64编码
+    }
+
+    /**
+     * 生成签名内容
+     * @param array $sign_data
+     * @return string
+     */
+    private function getSignContent($sign_data)
+    {
+        return json_encode($sign_data, JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * 生成随机字符串
+     * @param int $length
+     * @return string
+     */
+    protected function createNonceStr($length = 16)
+    {
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $str = '';
+        for ($i = 0; $i < $length; $i++) {
+            $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+        }
+        return $str;
+    }
+
+    /**
+     * 清理签名验证不必要的参数
+     * @return bool
+     */
+    protected function unsetTradeTypeAndNotifyUrl()
+    {
+        unset($this->config['notify_url']);
+        unset($this->config['trade_type']);
+        return true;
+    }
+}
---
+++ src/Gateways/Bhecard/Wap.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace Ansuns\Pay\Gateways\Bhecard;
+
+use Ansuns\Pay\Gateways\Bhecard;
+
+/**
+ * 微信WAP网页支付网关
+ * Class WapGateway
+ * @package Pay\Gateways\Wechat
+ */
+class Wap extends Bhecard
+{
+
+    /**
+     * 当前操作类型
+     * @return string
+     */
+    protected function getTradeType()
+    {
+        return 'MWEB';
+    }
+
+    /**
+     * 应用并生成参数
+     * @param array $options
+     * @param string $return_url
+     * @return string
+     * @throws \Ansuns\Pay\Exceptions\GatewayException
+     */
+    public function apply(array $options = [], $return_url = '')
+    {
+        $data = $this->preOrder($options);
+        $data['mweb_url'] = isset($data['mweb_url']) ? $data['mweb_url'] : '';
+        if (empty($return_url)) {
+            $return_url = $this->userConfig->get('return_url');
+        }
+        return $data['mweb_url'] . "&redirect_url=" . urlencode($return_url);
+    }
+}
