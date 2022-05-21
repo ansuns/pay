1b79895ee6c130c2eef237414f38e16c1449cf6e
sfc-ys <ys@suntekcorp.com>
2022-05-21 10:22:35 +0800

    test

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ansuns\Pay\Tests;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+    protected function setUp(): void
+    {
+
+    }
+
+    protected function tearDown(): void
+    {
+    }
+}
